3410e5740be74d2de0e2618632df13821e4b89ae
Sahas <sahasvivek001@gmail.com>
2022-06-20 13:04:58 +0530

    swapping alternate elements of array

---
+++ Arrays/alternateswap.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int swapalternates(int a[], int size)
+{
+    int temp = 0;
+    for (int i = 0; i < size; i += 2)
+    {
+        for (int j = i + 1; j <= i + 1; j++)
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[100], num;
+    cout << "Enter a number";
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cout << "Enter element " << i + 1 << " of array";
+        cin >> arr[i];
+    }
+
+    swapalternates(arr, num);
+}
