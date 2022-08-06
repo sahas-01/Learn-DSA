7b9251e788e1125cb867465ac2de3ea89efc56df
Sahas <sahasvivek001@gmail.com>
2022-08-06 19:52:22 +0530

    feat: pointer code update

--- Pointers/Pointer.cpp
+++ Pointers/Pointer.cpp
@@ -5,6 +5,8 @@ int main()
 {
     int n = 20;
     int *p = &n;
+    cout << p << endl;
+    cout << *p << endl;
     // p++;
     // *p = 30;
     // cout << p << endl;
@@ -12,10 +14,10 @@ int main()
     // cout << &n << endl;
 
     // Pointer to pointer
-    int **pp = &p; //Pointer which is pointing to the address of p
-    cout << pp << endl; //Address of pp
-    cout << *pp << endl; //Address of p
-    cout << **pp << endl; //Value of p
-    cout << *(*pp) << endl; //Value of p
+    int **pp = &p;          // Pointer which is pointing to the address of p
+    cout << pp << endl;     // Address of pp
+    cout << *pp << endl;    // Address of p
+    cout << **pp << endl;   // Value of p
+    cout << *(*pp) << endl; // Value of p
     return 0;
 }
